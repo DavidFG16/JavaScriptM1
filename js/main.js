6b0427c3f4d95f1057140bfe4b7949a83a34d7bb
DavidFG16 <189179517+DavidFG16@users.noreply.github.com>
2025-03-27 08:59:20 -0500

    feat: :construction: Función de registrar usuario y progreso de actualizar usuario

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// FUNCIONES PARA CRUD DE LOS PRODUCTOS 
 const saveProduct = async (data) => {
     const config = {
         method: "POST",
@@ -38,7 +39,55 @@ const findProducts = async (data) => {
     return result;
 }
 
+// FUNCIONES PARA CRUD DE LOS USUARIOS
+const saveUser = async (data) => {
+    const config = {
+        method: "POST",
+        body: JSON.stringify(data)
+    }
+    const response = await fetch('http://localhost:5600/users', config)
+    const result = await response.json()
+    return result;
+}
 
+const editUser = async (data) => {
+    const {id, ...dataUpdate} = data;
+    const config = {
+        method: "PATCH", // Actualizar 
+        body: JSON.stringify(dataUpdate)
+    }
+    const response = await fetch(`http://localhost:5600/users/${id}`, config)
+    const result = await response.json()
+    return result;
+}
+
+
+
+// MENU PARA CRUD DE LOS USUARIOS  
+// while(confirm("¿Quieres registrar un usuario?")){
+//     const data = {
+//         id: Number(prompt("Ingrese el ID","Mami-09")),
+//         name: prompt("Ingrese el nombre del usuario", "David"),
+//         last: prompt("Ingrese el apellido del usuario", "Gamboa")
+//     }
+//     saveUser(data)
+//     .then(result => alert(JSON.stringify(result)))
+//     .catch(error => alert(error))
+// }
+
+while(confirm("¿Quieres actualizar un usuario?")){
+    const data = {}
+    data.id= Number(prompt("Ingrese el ID del producto a actualizar","Mami-09" ))
+    data.name = (confirm("¿Desea cambiar el nombre del producto?")) ? prompt("Ingrese el nuevo nombre") : undefined
+    data.last = (confirm("Desea cambiar el apellido del usuario?")) ? prompt("Ingrese el nuevo apellido") : undefined
+    editUser(data)
+    .then(result => alert(JSON.stringify(result)))
+    .catch(error => alert(error))
+}
+
+
+
+// MENU PARA CRUD DE LOS PRODUCTOS ///////////////////////////////////////
 // while(confirm("¿Quieres guardar un producto?")){
 //     const data = {
 //         id: prompt("Ingrese el ID","Mami-09" ),
@@ -71,9 +120,10 @@ const findProducts = async (data) => {
 //     .catch(error => alert(error))
 // }
 
-while(confirm("¿Quieres obtener todos los productos?")){
-    findProducts()
-    .then(result => console.table((result)))
-    .catch(error => alert(error))
-    break
-}
+// while(confirm("¿Quieres obtener todos los productos?")){
+//     findProducts()
+//     .then(result => console.table((result)))
+//     .catch(error => alert(error))
+//     break
+// }
+///////////////////////////////////////////////////////
