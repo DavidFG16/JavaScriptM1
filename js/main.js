d4ec385980cf0e2ebf04a909d951ea5616b78baf
DavidFG16 <189179517+DavidFG16@users.noreply.github.com>
2025-03-28 08:49:09 -0500

    feat: :alien: Función Eliminar productos con el servidor MockAPI

--- js/main.js
+++ js/main.js
@@ -32,6 +32,18 @@ const editProduct = async (data) => {
     return result;
 }
 
+const removeProduct = async (data) => {
+    const {id} = data;
+    const url = new URL("https://67e686846530dbd311105602.mockapi.io");
+    url.pathname = `/products/${id}`;
+    const config = {
+        method: "DELETE", // Actualizar 
+    }
+    const response = await fetch(url.toString(), config)
+    const result = await response.json()
+    return result;
+}
+
 
 
 
